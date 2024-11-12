f77d9ff6730f37b67b9d5e739e5cf53ef10481c6
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2024-11-12 14:57:09 +0700

    Update track_uam.sh

--- track_uam.sh
+++ track_uam.sh
@@ -13,7 +13,7 @@ while true; do
     fi
 done
 echo -e "${GREEN}Current Block: $currentblock${NC}"
-block=$((currentblock - 4))
+block=$((currentblock - 3))
 sudo chmod 666 /var/run/docker.sock
 PBKEY=$(docker exec uam_1 printenv PBKEY)
 totalThreads=$(docker ps | grep debian:bullseye-slim | wc -l)
