d220b6f38c9ce7761a3959acc63f670c9aa06f6e
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2024-12-10 13:53:38 +0700

    Update exec_track_uam.sh

--- exec_track_uam.sh
+++ exec_track_uam.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo pkill -f track_uam.sh
 sudo rm -f track_uam.sh
 sudo rm -f $(pwd)/uam_log.txt
 wget --no-check-certificate https://github.com/anhtuan9414/temp-2/raw/main/track_uam.sh
