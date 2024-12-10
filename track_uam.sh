45d7dbf2278d34b7b9ce7de7a832cfdf2ea94ef9
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2024-12-10 14:33:49 +0700

    Update track_uam.sh

--- track_uam.sh
+++ track_uam.sh
@@ -109,5 +109,5 @@ if [ ${#restarted_threads[@]} -gt 0 ]; then
     for thread in "${restarted_threads[@]}"; do
         thread_list+="- $thread%0A"
     done
-    send_telegram_notification "$nowDate%0A%0AIP: $PUBLIC_IP%0AISP: $ISP%0AORG: $ORG%0ATOTAL THREADS: $totalThreads%0ARESTARTED THREADS: $numberRestarted%0A$thread_list"
+    send_telegram_notification "$nowDate%0A%0AIP: $PUBLIC_IP%0AISP: $ISP%0AORG: $ORG%0ACURRENT BLOCK: $currentblock%0ATOTAL THREADS: $totalThreads%0ARESTARTED THREADS: $numberRestarted%0A$thread_list"
 fi
