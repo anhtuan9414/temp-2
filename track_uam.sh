43b27a76b8192de75c9ed1397bff340faa0a9a08
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2024-12-12 00:44:46 +0700

    Update track_uam.sh

--- track_uam.sh
+++ track_uam.sh
@@ -73,7 +73,7 @@ if [ "${disk_usage%\%}" -ge 90 ]; then
     send_telegram_notification "$nowDate%0A%0ALOW AVAILABLE DISK WARNING!!!%0A%0AIP: $PUBLIC_IP%0AISP: $ISP%0AORG: $ORG%0ACOUNTRY: $COUNTRY%0AREGION: $REGION%0ACITY: $CITY%0A%0ASystem Information:%0A----------------------------%0AOS: $os_name%0ATotal CPU Cores: $cpu_cores%0ACPU Load (1-minute average): $cpu_load%%0ATotal RAM: $total_ram MB%0AAvailable RAM: $available_ram MB%0ADisk Usage (Root): $disk_usage"
 fi
 
-if [ "$available_ram" -le 500 ]; then
+if [ "$(echo "$available_ram" | awk '{print int($1 + 0.5)}')" -le 500 ]; then
     echo "LOW AVAILABLE RAM WARNING!!!"
     send_telegram_notification "$nowDate%0A%0ALOW AVAILABLE RAM WARNING!!!%0A%0AIP: $PUBLIC_IP%0AISP: $ISP%0AORG: $ORG%0ACOUNTRY: $COUNTRY%0AREGION: $REGION%0ACITY: $CITY%0A%0ASystem Information:%0A----------------------------%0AOS: $os_name%0ATotal CPU Cores: $cpu_cores%0ACPU Load (1-minute average): $cpu_load%%0ATotal RAM: $total_ram MB%0AAvailable RAM: $available_ram MB%0ADisk Usage (Root): $disk_usage"
 fi
