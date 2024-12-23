0ccfa3c0933c216bc301601fe7aed17853169444
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2024-12-23 15:53:07 +0700

    Update remove_bless_node.sh

--- remove_bless_node.sh
+++ remove_bless_node.sh
@@ -17,9 +17,7 @@ HEADERS=(
   -H "authorization: $AUTH_TOKEN"
   -H "content-type: application/json"
   -H "priority: u=1, i"
-  -H "sec-ch-ua: \"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\""
-  -H "sec-ch-ua-mobile: ?0"
-  -H "sec-ch-ua-platform: \"Windows\""
+  -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
   -H "sec-fetch-dest: empty"
   -H "sec-fetch-mode: cors"
   -H "sec-fetch-site: cross-site"
@@ -46,30 +44,23 @@ process_nodes() {
     exit 1
   else
     # Count the number of nodes by counting the occurrences of curly braces
-    node_count=$(echo "$response" | grep -o '{' | wc -l)
+    node_count=$(echo "$response" | jq length)
     echo "Number of nodes: $node_count"
-  # Initialize an empty array to store the pubKeys of nodes to retire
-  nodes_to_retire=()
-
-  # Filter nodes where isConnected=false and isRetired=false
-  echo "$response" | tr -d '\n' | grep -oE '{[^}]+}' | while IFS= read -r node; do
-    isConnected=$(echo "$node" | grep -o '"isConnected":false')
-    isRetired=$(echo "$node" | grep -o '"isRetired":false')
-    pubKey=$(echo "$node" | grep -o '"pubKey":"[^"]*' | awk -F':' '{print $2}' | tr -d '"')
-
-    # Check if both conditions are true and if pubKey exists
-    if [[ -n "$isConnected" && -n "$isRetired" && -n "$pubKey" ]]; then
-      # Add the pubKey to the nodes_to_retire array
-      nodes_to_retire+=("$pubKey")
-    fi
-  done
-  # Count the number of nodes in the nodes_to_retire array
-  nodes_to_retire_count=${#nodes_to_retire[@]}
-  echo "Number of nodes to retire: $nodes_to_retire_count"
-  # Loop through the list of nodes to retire and call retire_node for each
-  for pubKey in "${nodes_to_retire[@]}"; do
-    retire_node "$pubKey"
-  done
+    echo "$response" | jq -c '.[]' | while read -r node; do
+      # Extract the pubKey, isConnected, and isRetired fields using jq
+      pubKey=$(echo "$node" | jq -r '.pubKey')
+      isConnected=$(echo "$node" | jq -r '.isConnected')
+      isRetired=$(echo "$node" | jq -r '.isRetired')
+    
+      # Output the values
+      echo "pubKey: $pubKey, isConnected: $isConnected, isRetired: $isRetired"
+    
+      # Check if isConnected is false and isRetired is false
+      if [[ "$isConnected" == "false" && "$isRetired" == "false" ]]; then
+        # Add the pubKey to the nodes_to_retire array
+        retire_node "$pubKey"
+      fi
+    done
   fi
 }
 
