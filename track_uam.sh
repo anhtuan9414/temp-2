31b98ea02e7bc95b9387afe880dfb970a0625fab
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2025-02-20 16:34:09 +0700

    Update track_uam.sh

--- track_uam.sh
+++ track_uam.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 nowDate=$(date +"%Y-%m-%d %H:%M:%S %Z")
 echo $nowDate
+imageName=debian:bullseye-slim
+baseComposeUrl="https://github.com/anhtuan9414/uam-docker/raw/master/uam-swarm"
 
 sudo sed -ie 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1'/g /etc/sysctl.conf
 sudo chmod 666 /var/run/docker.sock
@@ -176,7 +178,7 @@ fi
 
 echo -e "${GREEN}Current Block: $currentblock${NC}"
 block=$((currentblock - 48))
-totalThreads=$(docker ps | grep debian:bullseye-slim | wc -l)
+totalThreads=$(docker ps | grep $imageName | wc -l)
 oldTotalThreads=$totalThreads
 
 echo "PBKEY: $PBKEY"
@@ -220,7 +222,7 @@ if [ "$setNewThreadUAM" -gt 0 ]; then
     send_telegram_notification "$nowDate%0A%0A ⚠️⚠️ LOW THREAD UAM WARNING!!!%0A%0AIP: $PUBLIC_IP%0AISP: $ISP%0AOrg: $ORG%0ACountry: $COUNTRY%0ARegion: $REGION%0ACity: $CITY%0A%0A✅ System Information:%0A----------------------------%0AOS: $os_name%0ATotal CPU Cores: $cpu_cores%0ACPU Load: $cpu_load%%0ATotal RAM: $total_ram MB%0ARAM Usage: $ram_usage%%0AAvailable RAM: $available_ram MB%0ADisk Usage (Root): $disk_usage%0A%0A✅ UAM Information:%0A----------------------------%0APBKey: $PBKEY%0A%0AIncreased the number of threads: $oldTotalThreads -> $totalThreads."
 fi
 
-allthreads=$(docker ps --format '{{.Names}}|{{.Status}}' --filter ancestor=debian:bullseye-slim | awk -F\| '{print $1}')
+allthreads=$(docker ps --format '{{.Names}}|{{.Status}}' --filter ancestor=$imageName | awk -F\| '{print $1}')
 
 restarted_threads=()
 numberRestarted=0
@@ -236,7 +238,7 @@ for val in $allthreads; do
     fi
 done
 
-threads=$(docker ps --format '{{.Names}}|{{.Status}}' --filter ancestor=debian:bullseye-slim | grep -e "3 days" -e "4 days" -e "5 days" -e "6 days" -e "7 days" -e "8 days" -e "9 days" -e "10 days" -e "11 days" -e "12 days" -e "13 days" -e "14 days" -e "15 days" -e "16 days"  -e "17 days" -e "18 days" -e "19 days" -e "20 days" -e "21 days" -e "22 days" -e "23 days" -e "24 days" -e "25 days" -e "26 days" -e "27 days" -e "28 days" -e "29 days" -e "30 days" -e "31 days" -e "2 weeks" -e "1 weeks" -e "1 week" -e "3 weeks" -e "4 weeks" -e "5 weeks" -e "6 weeks" -e "7 weeks" -e "8 weeks" -e "9 weeks" -e "10 weeks" -e "11 weeks" -e "12 weeks" -e "13 weeks" -e "1 months" -e "2 months" -e "3 months" -e "4 months" -e "5 months" -e "6 months" -e "7 months" -e "8 months" -e "9 months" -e "10 months" -e "11 months" -e "12 months" -e "1 years" -e "1 year" -e "2 years" -e "3 years" -e "4 years" -e "5 years" | awk -F\| '{print $1}')
+threads=$(docker ps --format '{{.Names}}|{{.Status}}' --filter ancestor=$imageName | grep -e "3 days" -e "4 days" -e "5 days" -e "6 days" -e "7 days" -e "8 days" -e "9 days" -e "10 days" -e "11 days" -e "12 days" -e "13 days" -e "14 days" -e "15 days" -e "16 days"  -e "17 days" -e "18 days" -e "19 days" -e "20 days" -e "21 days" -e "22 days" -e "23 days" -e "24 days" -e "25 days" -e "26 days" -e "27 days" -e "28 days" -e "29 days" -e "30 days" -e "31 days" -e "2 weeks" -e "1 weeks" -e "1 week" -e "3 weeks" -e "4 weeks" -e "5 weeks" -e "6 weeks" -e "7 weeks" -e "8 weeks" -e "9 weeks" -e "10 weeks" -e "11 weeks" -e "12 weeks" -e "13 weeks" -e "1 months" -e "2 months" -e "3 months" -e "4 months" -e "5 months" -e "6 months" -e "7 months" -e "8 months" -e "9 months" -e "10 months" -e "11 months" -e "12 months" -e "1 years" -e "1 year" -e "2 years" -e "3 years" -e "4 years" -e "5 years" | awk -F\| '{print $1}')
 
 for val in $threads; do 
     lastblock=$(docker logs $val --tail 500 2>&1 | grep -v "sendto: Invalid argument" | awk '/Processed block/ {block=$NF} END {print block}')
@@ -263,7 +265,7 @@ done
 # Function to download a file with retries
 download_file() {
     local file_name=$1
-    local url="https://github.com/anhtuan9414/uam-docker/raw/master/uam-swarm/$file_name"
+    local url="$baseComposeUrl/$file_name"
     local output=$file_name
     local wait_seconds=3
     local retry_count=0
