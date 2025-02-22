daf117551e41682bcc25e7b358013e3c56761267
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2025-02-22 14:33:01 +0700

    Update track_uam_hub.sh

--- track_uam_hub.sh
+++ track_uam_hub.sh
@@ -232,8 +232,8 @@ for val in $threads; do
         #sudo docker restart $val
         #echo -e "${RED}Restart: $val -Not activated after 45 hours${NC}"
         sudo docker rm -f $val
-        echo -e "${RED}Remove: $val - Not activated after 35 hours${NC}"
-        restarted_threads+=("$val - Not activated after 35 hours")
+        echo -e "${RED}Remove: $val - Not activated after 40 hours${NC}"
+        restarted_threads+=("$val - Not activated after 40 hours")
         ((numberRestarted+=1))
     elif [ "$lastblock" -le "$block" ]; then 
         #sudo docker restart $val
