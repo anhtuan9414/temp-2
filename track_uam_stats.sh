e5257f4786ddd24689779e5e3e395c718e1e4f0b
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2025-04-04 16:03:51 +0700

    Update track_uam_stats.sh

--- track_uam_stats.sh
+++ track_uam_stats.sh
@@ -3,6 +3,9 @@ nowDate=$(date +"%Y-%m-%d %H:%M:%S %Z")
 echo $nowDate
 API_URL="$1"
 API_KEY="$2"
+# Telegram Bot Configuration
+BOT_TOKEN="$3"
+CHAT_ID="$4"
 
 # Colors for output
 RED='\033[0;31m'
@@ -20,7 +23,7 @@ get_current_block_self() {
         fromBlock=184846
     fi
     while [ $retry_count -lt $max_retries ]; do
-        data=$(curl -s -X POST $API_URL/api/1.0 \
+        local data=$(curl -s -X POST $API_URL/api/1.0 \
             -H "Content-Type: application/json" \
             -d '{
                 "method": "getMiningBlocksWithTreasury",
@@ -32,10 +35,11 @@ get_current_block_self() {
             }')
     
         if [ -n "$data" ] && [ "$data" != "null" ]; then
+            lastBlockTime=$(date -d "$(echo $data | grep -oP '"dateTime":\s*"\K[^"]+') +7 hours" +"%d-%m-%Y %H:%M")
             lastBlock=$(echo $data | grep -oP '"id":\s*\K\d+')
             miningThreads=$(echo $data | grep -oP '"involvedInCount":\s*\K\d+')
             totalMiningThreads=$(echo $data | grep -oP '"numberMiners":\s*\K\d+')
-            rewardPerThread=$(echo $data | grep -oP '"price":\s*\K\d+')
+            rewardPerThread=$(echo $data | grep -oP '"price":\s*\K\d+\.\d+')
             break
         else
             retry_count=$((retry_count + 1))
@@ -45,11 +49,51 @@ get_current_block_self() {
     done
 }
 
+get_balance_self() {
+    local fromBlock=$(cat lastBlockStats.txt 2>/dev/null)
+    if [ -z "$fromBlock" ] || [ "$fromBlock" == "null" ]; then
+        fromBlock=184846
+    fi
+    while [ $retry_count -lt $max_retries ]; do
+        local data=$(curl -s -X POST $API_URL/api/1.0 \
+            -H "Content-Type: application/json" \
+            -d '{
+                "method": "getBalance",
+                "params": {
+                    "currency": "CRP"
+                },
+                "token": "'"$API_KEY"'"
+            }')
+    
+        if [ -n "$data" ] && [ "$data" != "null" ]; then
+            balance=$(echo $data | grep -oP '"result":\s*\K\d+')
+            break
+        else
+            retry_count=$((retry_count + 1))
+            echo "Attempt $retry_count/$max_retries failed to fetch current block. Retrying in 10 seconds..."
+            sleep 10
+        fi
+    done
+}
+
+
+# Function to send a Telegram notification
+send_telegram_notification() {
+    local message="$1"
+    curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
+        -d chat_id="$CHAT_ID" \
+        -d text="$message" > /dev/null
+}
+
 get_current_block_self
+get_balance_self
 
 echo $lastBlock > lastBlockStats.txt
-
+echo -e "${GREEN}Last Block Time: $lastBlockTime${NC}"
 echo -e "${GREEN}Last Block: $lastBlock${NC}"
 echo -e "${GREEN}Mining Threads: $miningThreads${NC}"
 echo -e "${GREEN}Reward Per Thread: $rewardPerThread${NC}"
 echo -e "${GREEN}Total Mining Threads: $totalMiningThreads${NC}"
+echo -e "${GREEN}CRP Balance: $balance${NC}"
+
+send_telegram_notification "$nowDate%0A%0A⛏️ MINING STATS%0A%0A🍀 Last Block Time: $lastBlockTime%0A🍀 Last Block: $lastBlock%0A🍀 Mining Threads: $miningThreads%0A🍀 Reward Per Thread: $rewardPerThread%0A🍀 Total Mining Threads: $totalMiningThreads%0A🍀 CRP Balance: $balance"
